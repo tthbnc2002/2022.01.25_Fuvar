c3a12ffe5c3605dbe1488e452d6fdc83e90685d8
tthbnc2002 <tothb.12d@acsjszki.hu>
2022-01-25 13:12:00 +0100

    add class Fuvar

---
+++ 2022.01.25_Fuvar/Fuvar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022._01._25_Fuvar
+{
+    class Fuvar
+    {
+        public int TaxiID { get; set; }
+        public string Indulas { get; set; }
+        public int Idotartam { get; set; }
+        public double Távolság { get; set; }
+        public double Viteldíj { get; set; }
+        public double Borravaló { get; set; }
+        public string FizetésMód { get; set; }
+
+        public Fuvar(string sor)
+        {
+            string[] t = sor.Split(';');
+            TaxiID = int.Parse(t[0]);
+            Indulas = t[1];
+            Idotartam = int.Parse(t[2]);
+            Távolság = double.Parse(t[3]);
+            Viteldíj = double.Parse(t[4]);
+            Borravaló = double.Parse(t[5]);
+            FizetésMód = t[6];
+        }
+    }
+}
