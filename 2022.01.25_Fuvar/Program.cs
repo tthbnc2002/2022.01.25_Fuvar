0a3b3cfcb767db56aa62c4f32e1e814eb17d93b9
tthbnc2002 <tothb.12d@acsjszki.hu>
2022-01-25 12:56:24 +0100

    Add project files.

---
+++ 2022.01.25_Fuvar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022._01._25_Fuvar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
