806740663fdfa972635ea7c4d055fe9061cad07d
tthbnc2002 <tothb.12d@acsjszki.hu>
2022-02-08 12:42:45 +0100

    xdxxdxd

--- 2022.01.25_Fuvar/Fuvar.cs
+++ 2022.01.25_Fuvar/Fuvar.cs
@@ -4,28 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2022._01._25_Fuvar
+namespace _2022_01_24_Fuvar
 {
     class Fuvar
     {
-        public int TaxiID { get; set; }
-        public string Indulas { get; set; }
-        public int Idotartam { get; set; }
-        public double Távolság { get; set; }
-        public double Viteldíj { get; set; }
-        public double Borravaló { get; set; }
-        public string FizetésMód { get; set; }
-
-        public Fuvar(string sor)
+        public int taxiAzonosito { get; set; }
+        public string utazasIdopontja { get; set; }
+        public int utazasIdotartama { get; set; }
+        public double megtettTavolsag { get; set; }
+        public double viteldij { get; set; }
+        public double borravalo { get; set; }
+        public string fizetesMod { get; set; }
+        public Fuvar(string s)
         {
-            string[] t = sor.Split(';');
-            TaxiID = int.Parse(t[0]);
-            Indulas = t[1];
-            Idotartam = int.Parse(t[2]);
-            Távolság = double.Parse(t[3]);
-            Viteldíj = double.Parse(t[4]);
-            Borravaló = double.Parse(t[5]);
-            FizetésMód = t[6];
+            string[] db = s.Split(';');
+            taxiAzonosito = int.Parse(db[0]);
+            utazasIdopontja = db[1];
+            utazasIdotartama = int.Parse(db[2]);
+            megtettTavolsag = double.Parse(db[3]);
+            viteldij = double.Parse(db[4]);
+            borravalo = double.Parse(db[5]);
+            fizetesMod = db[6];
         }
     }
 }
--- 2022.01.25_Fuvar/Program.cs
+++ 2022.01.25_Fuvar/Program.cs
@@ -5,12 +5,77 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace _2022._01._25_Fuvar
+namespace _2022_01_24_Fuvar
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Fuvar> fuvarok = new List<Fuvar>();
+            File.ReadAllLines("fuvar.csv").Skip(1).ToList().ForEach((s) => fuvarok.Add(new Fuvar(s)));
+
+            Console.WriteLine($"3. feladat: {fuvarok.Count} fuvar");
+
+            int fuvarSzam = fuvarok.Where((f) => f.taxiAzonosito == 6185).Count();
+            double fuvarBevetel = 0;
+            fuvarok.ForEach((f) => {
+                if (f.taxiAzonosito == 6185)
+                {
+                    fuvarBevetel += (f.borravalo + f.viteldij);
+                }
+            });
+
+            Console.WriteLine($"4. feladat: {fuvarSzam} alatt: {fuvarBevetel}$");
+
+            /*Dictionary<string, int> engedelyezettFizetesiModok = new Dictionary<string, int>();
+            fuvarok.ForEach((f) => {
+                if (!engedelyezettFizetesiModok.ContainsKey(f.fizetesMod)) {
+                    engedelyezettFizetesiModok.Add(f.fizetesMod, 1);
+                } else {
+                    engedelyezettFizetesiModok[f.fizetesMod]++;
+                }
+            });
+            Console.WriteLine("5. feladat:");
+            foreach (var e in engedelyezettFizetesiModok) {
+                Console.WriteLine($"\t{e.Key}: {e.Value} fuvar");
+            }*/
+
+            fuvarok.GroupBy((f) => f.fizetesMod).Select((x) => new {
+                fizetésMód = x.Key,
+                db = x.Count()
+            }).ToList().ForEach((f) => {
+                Console.WriteLine($"\t{f.fizetésMód}: {f.db} fuvar");
+            });
+
+            double osszKilometer = 0;
+
+            fuvarok.ForEach((f) => {
+                osszKilometer += (f.megtettTavolsag * 1.6);
+            });
+
+            Console.WriteLine($"6. feladat: {osszKilometer.ToString("F2")}km");
+
+            Fuvar leghosszabbFuvar = fuvarok.OrderBy((f) => f.utazasIdotartama).Last();
+
+            Console.WriteLine("7. feladat: Leghosszabb fuvar:");
+            Console.WriteLine($"\tFuvar hossza: {leghosszabbFuvar.utazasIdotartama} másodperc");
+            Console.WriteLine($"\tTaxi azonosító: {leghosszabbFuvar.taxiAzonosito}");
+            Console.WriteLine($"\tMegtett távolság: {leghosszabbFuvar.megtettTavolsag.ToString("F1")} km");
+            Console.WriteLine($"\tViteldíj: {leghosszabbFuvar.viteldij}$");
+
+            Console.WriteLine("8. feladat: hiba.txt");
+            List<string> kiirandoHibak = new List<string>();
+            kiirandoHibak.Add("taxi_id;indulas;idotartam;tavolsag;viteldij;borravalo;fizetes_modja");
+
+            fuvarok.ForEach((f) => {
+                if (f.utazasIdotartama > 0 && f.viteldij > 0 && f.megtettTavolsag == 0)
+                {
+                    kiirandoHibak.Add($"{f.taxiAzonosito};{f.utazasIdopontja};{f.utazasIdotartama};{f.megtettTavolsag};{f.viteldij};{f.borravalo};{f.fizetesMod}");
+                }
+            });
+
+            File.WriteAllLines("hiba.txt", kiirandoHibak);
+
             Console.ReadKey();
         }
     }
